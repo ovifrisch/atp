6a0690c9018fcc55ec6ae86e951f759d752bc2b1
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-08-29 20:38:22 -0700

    Updates

---
+++ precache-manifest.2701966702573b03a5c7e7cd08b77072.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277229562c96da94e6e2ae685d82537b",
+    "url": "/atp/index.html"
+  },
+  {
+    "revision": "b8d96a7afb12a72620c2",
+    "url": "/atp/static/css/2.77770c2e.chunk.css"
+  },
+  {
+    "revision": "1c1eabde601b881eeed0",
+    "url": "/atp/static/css/main.d24b4a74.chunk.css"
+  },
+  {
+    "revision": "b8d96a7afb12a72620c2",
+    "url": "/atp/static/js/2.adf6b205.chunk.js"
+  },
+  {
+    "revision": "1c1eabde601b881eeed0",
+    "url": "/atp/static/js/main.fba3589a.chunk.js"
+  },
+  {
+    "revision": "3fea9b66e9e05b2dd396",
+    "url": "/atp/static/js/runtime~main.038e185b.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/atp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/atp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/atp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/atp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/atp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/atp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/atp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/atp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/atp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/atp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/atp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/atp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/atp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/atp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/atp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/atp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
